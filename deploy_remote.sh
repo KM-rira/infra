ec9ba52ea740aef1e90840abcadd97b06b2c2217
Koji <>
2026-03-31 03:06:56 +0900

    add pull

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -11,6 +11,7 @@ ssh -i "${SSH_KEY_PATH}" -t "${TARGET_INSTANCE}" "
   export GH_USER='${GH_USER}'
   export GH_PAT='${GH_PAT}'
   cd '${TARGET_INFRA_DIR}' &&
+  git pull &&
   echo \"\$GH_PAT\" | docker login ghcr.io -u \"\$GH_USER\" --password-stdin &&
   bash ./prd_compose_deploy.sh
 "
