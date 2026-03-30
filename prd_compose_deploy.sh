cc5ecd5f2d708f5b9898153685e06cc575dfe704
Koji <>
2026-03-31 02:31:28 +0900

    add hono-note-backend setting

--- prd_compose_deploy.sh
+++ prd_compose_deploy.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
-
 set -e
 
 echo "🔄 Pull latest images..."
 docker compose pull
 
-echo "🐘 Start postgres first..."
-docker compose up -d postgres
+echo "🐘 Start postgres, redis first..."
+docker compose up -d postgres redis
 
 echo "⏳ Waiting for PostgreSQL to be ready..."
-until docker compose exec -T postgres pg_isready -U "$POSTGRES_USER" > /dev/null 2>&1; do
+until docker compose exec -T postgres pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /dev/null 2>&1; do
   sleep 2
-  echo "⏳ waiting..."
+  echo "⏳ waiting for postgres..."
+done
+
+echo "⏳ Waiting for Redis to be ready..."
+until docker compose exec -T redis redis-cli ping | grep -q PONG; do
+  sleep 2
+  echo "⏳ waiting for redis..."
 done
 
 echo "🚀 Recreate app services..."
-docker compose up -d --no-deps --force-recreate growry linkhub
+docker compose up -d --no-deps --force-recreate growry linkhub hono-note-backend
 
 echo "🛠 Running DB init script..."
 docker compose exec -T growry npm run db:init
